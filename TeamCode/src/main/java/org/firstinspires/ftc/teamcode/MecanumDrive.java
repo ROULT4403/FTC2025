5dffab33368fc4a154c811b0ccfe79a96647497e
sebsa <sebsanor@gmail.com>
2025-10-07 19:22:12 -0600

    se agrego el tuning, aun falta tunear

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -226,10 +226,12 @@ public final class MecanumDrive {
 
         // TODO: make sure your config has motors with these names (or change them)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "FWDleftDrive");
+        leftBack = hardwareMap.get(DcMotorEx.class, "BCKleftDrive");
+        rightBack = hardwareMap.get(DcMotorEx.class, "BCKrightDrive");
+        rightFront = hardwareMap.get(DcMotorEx.class, "FWDright" +
+                "" +
+                "Drive");
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
