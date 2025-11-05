baa72bcdf9219895a83c57cfdfa711e183617ec8
sebsa <sebsanor@gmail.com>
2025-11-04 18:52:28 -0600

    Empieza progra para temporada, ganaresmo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/URSS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/URSS.java
@@ -1,6 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
-public class URSS {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+public class URSS extends LinearOpMode {
+    DcMotor arm = null;
+    DcMotor intake = null;
 
+    @Override
+    public void runOpMode() {
+
+        intake = hardwareMap.get(DcMotor.class, "shooter");
+        intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        waitForStart();
+
+        if (gamepad1.a) {
+            if (intake.getPower() == 0.8) {
+                intake.setPower(0);
+            } else {
+                intake.setPower(.8);
+            }
+
+        } else if (gamepad1.b) {
+            if (intake.getPower() == -0.8) {
+                intake.setPower(0);
+            } else {
+                intake.setPower(-0.8);
+            }
+
+        }
+
+    }
 }
