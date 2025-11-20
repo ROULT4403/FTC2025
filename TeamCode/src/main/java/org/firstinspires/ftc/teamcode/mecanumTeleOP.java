126a3aef6e21c9abf0b715fa6220b004d0666ed2
sebsa <sebsanor@gmail.com>
2025-11-20 17:55:51 -0600

    código base del robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumTeleOP.java
@@ -3,6 +3,24 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="MecanumTeleOP", group="Linear Opmode")
 public class mecanumTeleOP extends LinearOpMode {
 
